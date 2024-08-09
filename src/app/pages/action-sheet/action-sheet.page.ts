c5382e6596671f1aa0cedc0c715d99fa7b6f900a
anme-22 <angeldavidmendez2012@gmail.com>
2024-08-09 14:51:44 -0600

    adding alerts and action sheets

--- src/app/pages/action-sheet/action-sheet.page.ts
+++ src/app/pages/action-sheet/action-sheet.page.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
+import { Icon } from 'ionicons/dist/types/components/icon/icon';
 
 @Component({
   selector: 'app-action-sheet',
   templateUrl: './action-sheet.page.html',
   styleUrls: ['./action-sheet.page.scss'],
 })
-export class ActionSheetPage implements OnInit {
+export class ActionSheetPage {
+  constructor(private actionSheetCtrl: ActionSheetController) {}
 
-  constructor() { }
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: 'Actions',
+      buttons: [
+        {
+          text: 'Delete',
+          role: 'destructive',
+          data: {
+            action: 'delete',
+          },
+          handler: () => {
+            console.log('The item was deleted');
+          },
+        },
+        {
+          text: 'Share',
+          data: {
+            action: 'share',
+          },
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          data: {
+            action: 'cancel',
+          },
+        },
+      ],
+    });
 
-  ngOnInit() {
+    await actionSheet.present();
   }
-
 }
--- src/app/pages/alert/alert.page.ts
+++ src/app/pages/alert/alert.page.ts
@@ -1,15 +1,75 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-alert',
   templateUrl: './alert.page.html',
   styleUrls: ['./alert.page.scss'],
 })
-export class AlertPage implements OnInit {
+export class AlertPage {
+  constructor(private alertController: AlertController) {}
 
-  constructor() { }
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'A Short Title Is Best',
+      subHeader: 'A Sub Header Is Optional',
+      message: 'A message should be a short, complete sentence.',
+      buttons: ['OK'],
+    });
 
-  ngOnInit() {
+    await alert.present();
   }
 
+  async multipleButtons() {
+    const alert = await this.alertController.create({
+      header: 'Multiples buttons',
+      subHeader: 'A Sub Header Is Optional',
+      message: 'A message should be a short, complete sentence.',
+      buttons: [
+        {
+          text: 'OK',
+          handler: () => {
+            console.log('click in okay');
+          },
+        },
+        {
+          text: 'Cancelar',
+          role: 'destructive',
+          handler: () => {
+            console.log('click in cancelar');
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
+  async inputsButtons() {
+    const alert = await this.alertController.create({
+      header: 'Multiples buttons',
+      inputs: [
+        {
+          placeholder: 'Name',
+        },
+        {
+          placeholder: 'Nickname (max 8 characters)',
+          attributes: {
+            maxlength: 8,
+          },
+        },
+        {
+          type: 'number',
+          placeholder: 'Age',
+          min: 1,
+          max: 100,
+        },
+        {
+          type: 'textarea',
+          placeholder: 'A little about yourself',
+        },
+      ],
+    });
+
+    await alert.present();
+  }
 }
